c4760713af2a4791cc43f1e60778266e8dc8bb7f
shaobai-li <shaobai.ai@gmail.com>
2025-10-04 23:39:55 +0800

    feat(backend_agent): add query embedding as part of local search step in a dialog

---
+++ intent_recognition/search_agent.py
@@ -0,0 +1,29 @@
+import os
+import openai
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer 
+load_dotenv()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+
+system_prompt = ""
+
+embedding_model = SentenceTransformer('BAAI/bge-large-zh-v1.5')
+
+def local_search(queries, k):
+    queries_embedded = embedding_model.encode(queries)
+    return ["我是中国人", "我爱中国"]
+
+def main():
+    print(f"AI: 您好，今天的自媒体内容创作，您对什么样的选题感兴趣？")
+    user_content = input("You: ")
+    print(f"AI: 根据你的选题方向，我搜索一下本地数据库。")
+    texts = local_search(user_content, 4)
+    for text in texts:
+        print(f"AI: {text}")
+
+if __name__ == "__main__":
+    main()
