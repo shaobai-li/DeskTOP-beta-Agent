2fe895c7a7821195efcf2b54e3dd14d3a18e12bd
shaobai-li <shaobai.ai@gmail.com>
2025-10-05 23:57:14 +0800

    feat(backend_agent): Implement external mapping linking FAISS embeddings to text chunks

--- intent_recognition/search_agent.py
+++ intent_recognition/search_agent.py
@@ -1,4 +1,5 @@
 import os
+import json
 import openai
 import faiss
 from dotenv import load_dotenv
@@ -13,20 +14,28 @@ DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 system_prompt = ""
 
 embedding_model = SentenceTransformer('BAAI/bge-large-zh-v1.5')
-index = faiss.read_index("database/cg_chunks_20251014.index")
+cg_chunks_index = faiss.read_index("database/cg_chunks_20251014.index")
+with open("database/cg_chunks_20251014.json", "r", encoding="utf-8") as f:
+    cg_chunks_json = json.load(f)
+id_to_chunk = {c["id"]: c for c in cg_chunks_json}
 
 def local_search(queries, k):
     queries_embedded = embedding_model.encode([queries])
-    D, I = index.search(queries_embedded, k=10)
-    return ["我是中国人", "我爱中国"]
+    _, I = cg_chunks_index.search(queries_embedded, k=10)
+    
+    chunks = []
+    for idx in I[0]:
+        chunks.append(id_to_chunk[idx]["text"])
+    return chunks
 
 def main():
     print(f"AI: 您好，今天的自媒体内容创作，您对什么样的选题感兴趣？")
     user_content = input("You: ")
     print(f"AI: 根据你的选题方向，我搜索一下本地数据库。")
-    texts = local_search(user_content, 4)
-    for text in texts:
+    chunks = local_search(user_content, 4)
+    for text in chunks:
         print(f"AI: {text}")
+    
 
 if __name__ == "__main__":
     main()
