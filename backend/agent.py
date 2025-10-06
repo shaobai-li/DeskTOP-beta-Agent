1e24f322029f3eb15589b1d84383dc61c8a5ec76
shaobai-li <shaobai.ai@gmail.com>
2025-10-07 00:21:14 +0800

    fix(backend_agent): fix the error that codes for loading cg_chunks are outside of __init__() in SearchAgent

--- backend/agent.py
+++ backend/agent.py
@@ -3,12 +3,13 @@ import faiss
 from sentence_transformers import SentenceTransformer 
 
 class SearchAgent:
+    
     def __init__(self):
         self.embedding_model = SentenceTransformer('BAAI/bge-large-zh-v1.5')
         self.cg_chunks_index = faiss.read_index("database/cg_chunks_20251014.index")
-    with open("database/cg_chunks_20251014.json", "r", encoding="utf-8") as f:
-        cg_chunks_json = json.load(f)
-    id_to_chunk = {c["id"]: c for c in cg_chunks_json}
+        with open("database/cg_chunks_20251014.json", "r", encoding="utf-8") as f:
+            cg_chunks_json = json.load(f)
+        self.id_to_chunk = {c["id"]: c for c in cg_chunks_json}
 
     def local_search(self, queries, k):
         queries_embedded = self.embedding_model.encode([queries])
@@ -18,3 +19,4 @@ class SearchAgent:
         for idx in I[0]:
             chunks.append(self.id_to_chunk[idx]["text"])
         return chunks
+    
