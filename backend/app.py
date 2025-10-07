1e635456a94eaf9b0dc3c6b325c1f0c5a017b467
shaobai-li <shaobai.ai@gmail.com>
2025-10-07 19:29:49 +0800

    chore(backend): rename search_agent.py to app.py as backend entry file

---
+++ backend/app.py
@@ -0,0 +1,38 @@
+import os
+import json
+import openai
+import faiss
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer 
+load_dotenv()
+import utils
+
+print(utils.__file__)
+
+exit(0)
+
+from agent import SearchAgent
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+
+system_prompt = ""
+
+
+def main():
+
+    print("加载 search_agent ...")
+    search_agent = SearchAgent()
+    print("加载成功\n")
+
+    print(f"AI: 您好，今天的自媒体内容创作，您对什么样的选题感兴趣？")
+    user_content = input("You: ")
+    print(f"AI: 根据你的选题方向，我搜索一下本地数据库。")
+    chunks = search_agent.local_search(user_content, 4)
+    for text in chunks:
+        print(f"AI: {text}")
+    
+
+if __name__ == "__main__":
+    main()
