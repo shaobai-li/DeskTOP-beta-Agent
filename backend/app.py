5c4ccdc4b7f8cc4bc0e3e0a27b160c825e830be1
shaobai-li <shaobai.ai@gmail.com>
2025-10-20 00:34:36 +0800

    feat(backend): add file upload interface

--- backend/app.py
+++ backend/app.py
@@ -2,16 +2,18 @@
 import os
 import json
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
-
+from datetime import datetime
 from pydantic import BaseModel
 from agents import SearchAgent
 
 # 初始化
 app = FastAPI()
 search_agent = SearchAgent()
-DATA_PATH = Path(__file__).parent / "data.json"
+UPLOAD_DIR = Path(__file__).parent / "database/uploads"
+UPLOAD_DIR.mkdir(exist_ok=True)
+TEXT_METADATA_FILE = UPLOAD_DIR / "texts_metadata.json"
 
 # 定义请求体
 class UserQuery(BaseModel):
@@ -48,6 +50,34 @@ def generate_content(query: UserQuery):
 
 @app.get("/rows")
 def rows():
-    text = DATA_PATH.read_text(encoding="utf-8")
+    text = TEXT_METADATA_FILE.read_text(encoding="utf-8")
     data = json.loads(text)
     return data
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = UPLOAD_DIR / file.filename
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    file_info = {
+        "title": file.filename.split(".")[0],
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "status": "已导入"
+    }
+
+    if TEXT_METADATA_FILE.exists():
+        with open(TEXT_METADATA_FILE, "r", encoding="utf-8") as f:
+            records = json.load(f)
+    else:
+        records = []
+    records.append(file_info)
+    with open(TEXT_METADATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(records, f, ensure_ascii=False, indent=2)
+
+    return {
+        "message": "文件上传成功",
+        "filename": file.filename,
+    }
