9b9a23ce52e2ca3467911a95ed107392b21d0a1b
shaobai-li <shaobai.ai@gmail.com>
2025-11-12 15:08:56 +0800

    refactor(frontend_sidebar): restructure MenuItem component with BEM naming and optional feature icon

--- frontend/src/components/sidebar/MenuItem.jsx
+++ frontend/src/components/sidebar/MenuItem.jsx
@@ -1,18 +1,27 @@
     import { Link } from "react-router-dom";
+    import featureIcon from '../../assets/icons8-sidebar_ellipsis-h-30.png';
     import './MenuItem.css';
 
-    export default function MenuItem({ title, path, selectedItem, handleMenuItemClick, icon }) {
+    export default function MenuItem({ title, path, selectedItem, handleMenuItemClick, icon, hasFeature = false }) {
         const isSelected = selectedItem === title;
         const shouldRenderSelected = title !== '新聊天';
 
         return (
-            <Link 
-                to={path} 
-                className={`menu-item ${isSelected && shouldRenderSelected ? 'selected' : ''}`}
-                onClick={() => handleMenuItemClick(title)}
-            >
-                <img className="menu-item-icon" src={icon}></img>
-                <span className="menu-item-text">{title}</span>
-            </Link>
+            
+            <div className="menu-item">
+                <Link 
+                    to={path} 
+                    className={`menu-item__link ${isSelected && shouldRenderSelected ? 'selected' : ''}`}
+                    onClick={() => handleMenuItemClick(title)}
+                >
+                    <img className="menu-item__icon" src={icon}></img>
+                    <span className="menu-item__text">{title}</span>
+                </Link>
+                {hasFeature && (
+                    <div>
+                        <img className="menu-item__feature-icon" src={featureIcon}  />
+                    </div>
+                )}
+            </div>
         )
     }
