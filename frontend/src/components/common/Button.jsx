da2d8d471fd8e712f29141db2ba3140d2453e6fd
shaobai-li <shaobai.ai@gmail.com>
2025-11-26 17:44:22 +0800

    feat(frontend_common): enhance Button component to support circular icon buttons

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -1,15 +1,18 @@
 const themeStyles = {
-    black: "bg-neutral-900 text-neutral-50 hover:bg-neutral-800 active:bg-neutral-700",
-    white: "bg-white text-neutral-900 border border-neutral-200 shadow-sm hover:bg-neutral-100 active:bg-neutral-50",
+    black: "px-5 py-1 text-sm bg-neutral-900 text-neutral-50 hover:bg-neutral-800 active:bg-neutral-700",
+    white: "px-5 py-1 text-sm bg-white text-neutral-900 border border-neutral-200 shadow-sm hover:bg-neutral-100 active:bg-neutral-50",
+    blackCircle: "w-10 h-10 bg-neutral-900 text-neutral-50 hover:bg-neutral-800 active:bg-neutral-700 rounded-full",
+    whiteCircle: "w-10 h-10 bg-white text-neutral-900 border border-neutral-200 shadow-sm hover:bg-neutral-100 active:bg-neutral-50 rounded-full"
 };
 
-function Button({ onClick, text, theme = "black", disabled = false }) {
+function Button({ onClick, text, theme = "black", icon = null, disabled = false }) {
     return (
         <button
-            className={`flex items-center justify-center px-5 py-1 rounded-lg text-sm ${themeStyles[theme]} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
+            className={`flex items-center justify-center ${themeStyles[theme]} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
             onClick={onClick}
             disabled={disabled}
         >
+            {icon && <img src={icon} alt="icon" className="w-6 h-6"/>}
             {text}
         </button>
     );
