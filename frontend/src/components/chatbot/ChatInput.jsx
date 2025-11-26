7581709e313439e9ce6812feb663bffd7ad23ae4
shaobai-li <shaobai.ai@gmail.com>
2025-11-26 17:46:37 +0800

    feat(frontend_chats): integrate new Button component and add agent selector

--- frontend/src/components/chatbot/ChatInput.jsx
+++ frontend/src/components/chatbot/ChatInput.jsx
@@ -1,6 +1,8 @@
 import './ChatInput.css';
 import { useState, useRef, useEffect } from 'react';
 import sendButton from '@assets/icon-action-send.png';
+import agentIcon from '@assets/icon-ui-robot.svg';
+import Button from '../common/Button';
 
 export default function ChatInput({ onSendMessage }) {
   const [inputValue, setInputValue] = useState('');
@@ -20,6 +22,10 @@ export default function ChatInput({ onSendMessage }) {
     }
   }
 
+  const handleAgentSelector = () => {
+    console.log("agent selector");
+  }
+
   return (
     <div className="chat-input">
       <input
@@ -29,12 +35,19 @@ export default function ChatInput({ onSendMessage }) {
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      <div className="chat-button-container">
-        <button 
-        className="send-button"
-        onClick={handleSendMessage}>
-          <img src={sendButton} alt="Send" width={20} height={20}/>
-        </button>
+      <div className="flex flex-row justify-between px-2 py-2 gap-2">
+        <Button 
+          className="agent-selector-button"
+          onClick={handleAgentSelector}
+          icon={agentIcon}
+          theme="whiteCircle"
+        />
+        <Button 
+          className="send-button"
+          onClick={handleSendMessage}
+          icon={sendButton}
+          theme="blackCircle"
+        />
       </div>
     </div>
   )
